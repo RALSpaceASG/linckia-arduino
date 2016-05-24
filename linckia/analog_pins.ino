636cd40816012134190e127a7315429c83f7b185
Laurence de Bruxelles <laurence.de-bruxelles@stfc.ac.uk>
2016-05-24 15:51:53 +0100

    split analog pin reading commands into separate sketch

---
+++ linckia/analog_pins.ino
@@ -0,0 +1,51 @@
+//feedback
+int mfeedon;
+
+void AnalogPinSetup() {
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+  digitalWrite(A4, LOW);
+  digitalWrite(A5, LOW);
+
+  pinMode(2,OUTPUT);
+  digitalWrite(2, HIGH);
+}
+
+void ReadAnalog(uint8_t targetPin) {
+  int feedback = analogRead(targetPin);
+  byte feedback100 = feedback/100;
+  byte feedback10  = feedback - feedback100*100;
+  if (feedback100 < 100) {
+   if (feedback10 < 100) {
+     Return(1,targetPin,feedback100,feedback10);
+   }
+ }
+}
+
+void SensorInterval(byte command[6]) {
+  //TODO Start stop readout
+  if (command[1] == 1) { //command for changeg feedback interval
+    mfeedon = command[2]; //turn it on if 1 turn it off if 0
+    //command[3] not used
+    ulSensorsInterval = (command[4]*100)+command[5];
+    MSensors.interval(ulSensorsInterval);
+    MSensors.reset();
+  }
+}
+
+void CheckSensors() {
+  if(mfeedon == 1) {
+    static int j;
+    ReadAnalog(j);
+    j = j + 1;
+    if(j == 5) { j = 0; }
+  }
+}
--- linckia/linckia.ino
+++ linckia/linckia.ino
@@ -3,7 +3,7 @@
  *  Linckia Arduino Code
  * ------------------------------
  * Group: RAL Space Autonomous Systems Group
- * Contact: Aron Kisdi aron.kisdi@stfc.ac.uk
+ * Contact: Aron Kisdi <aron.kisdi@stfc.ac.uk>
  * Organisation: RAL Space, STFC
  * ------------------------------
  */
@@ -11,7 +11,7 @@
 #define LINCKIA_VERSION "6.6"
 #define LINCKIA_VERSION_DATE "24 May 2016"
 
-// Import the Arduino Servo library
+// imports
 #include <Metro.h>
 
 // compatibility with Arduino < 1.6.7
@@ -22,13 +22,6 @@ static const int routeren = 2; //Digital pin 2 for enabling 3.3V regulator
 // User commands over serial
 byte command[6];                        // raw input from serial buffer, 6 bytes
 enum command_t {MOTOR = 1, SERVO, SENSOR, SENSOR_INTERVAL, PING, REBOOT, VERSION};
-
-//feedback
-int feedback;             // reading from analog port
-int feedback10;           // variable for storing first two digits of feedback
-int feedback100;          // variable for storing 100s decimal places of feedback
-int analogpin;            // variable for setting which analog pin to read
-int mfeedon;
 
 //Metro objects for schedueled tasks
 unsigned long ulActuatorsInterval = 100;
@@ -46,57 +39,10 @@ void Return(int ID, int value, int value1, int value2) {
   Serial.write(254);
 }
 
-//USER DEFINED FUNCTIONS
-void PinSetup() {
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
-  digitalWrite(A0, LOW);
-  digitalWrite(A1, LOW);
-  digitalWrite(A2, LOW);
-  digitalWrite(A3, LOW);
-  digitalWrite(A4, LOW);
-  digitalWrite(A5, LOW);
-
-  pinMode(2,OUTPUT);
-  digitalWrite(2, HIGH);
-}
-
-void ReadAnalog(int targetPin) {
-  feedback = analogRead(targetPin);
-  feedback100 = feedback/100;
-  feedback10 = feedback-feedback100*100;
-  //Return(1,targetPin,feedback100,feedback10);
-  if (feedback100 < 100){
-   if (feedback10 < 100){
-     Return(1,targetPin,feedback100,feedback10);
-   }
-  }
-}
-
 void MoveActuators() {
   //Moves actuator by a calculated increment every 10 milliseconds
   moveServos();
   moveMotors();
-}
-
-void CheckSensors() {
-  if (MSensors.check() == 1) //time to check motor feedback
-  {
-    if(mfeedon == 1) //if it is on 0 is off 1 is on
-    {
-      static int j;
-      ReadAnalog(j);
-      j = j + 1;
-      if(j == 5)
-      {
-        j = 0;
-      }
-    }
-  }
 }
 
 //************************************
@@ -141,20 +87,12 @@ void HandleCommand(byte command[6]) {
       break;
     case SENSOR:
     {
-      analogpin = command[1];
-      ReadAnalog(analogpin);
+      ReadAnalog(command[1]);
       break;
     }
     case SENSOR_INTERVAL:
     {
-      //TODO Start stop readout
-      if (command[1] == 1) { //command for changeg feedback interval
-        mfeedon = command[2]; //turn it on if 1 turn it off if 0
-        //command[3] not used
-        ulSensorsInterval = (command[4]*100)+command[5];
-        MSensors.interval(ulSensorsInterval);
-        MSensors.reset();
-      }
+      SensorInterval(command);
       break;
     }
     case PING:
@@ -194,9 +132,13 @@ void HandleCommand(byte command[6]) {
 //********************************
 
 void setup() {
+  // router reset pin
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
   ServoSetup();
   MotorSetup();
-  PinSetup();  //set up I/O
+  AnalogPinSetup();  //set up I/O
   // Open the serial connection, baud 9600 (max: 115200)
   Serial.begin(9600);
 }
